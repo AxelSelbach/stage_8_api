05b929319da06e1b330cba13ba8b77c509b68102
axel <axelselbach90@gmail.com>
2023-09-11 17:47:25 -0300

    unstructured route params

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello, world!");
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params;
+
+  response.send(`
+  Mensagem ID: ${id}.
+  Para o usuário: ${user}.
+  
+  `)
 })
 
-const PORT = 3333;
+const PORT = 3333
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
