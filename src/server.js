82f227a79b61e9bbbcbbbce40caacbdab53f644f
axel <axelselbach90@gmail.com>
2023-09-11 17:57:39 -0300

    query params added to project

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 app.get('/message/:id/:user', (request, response) => {
   const { id, user } = request.params;
@@ -8,10 +8,15 @@ app.get('/message/:id/:user', (request, response) => {
   response.send(`
   Mensagem ID: ${id}.
   Para o usuário: ${user}.
-  
-  `)
-})
+  `);
+});
 
-const PORT = 3333
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+  response.send(`Página: ${page}. Mostrar: ${limit}`);
+});
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
