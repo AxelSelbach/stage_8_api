edcd0d04937ce5fddd62fffedc9871aa4967a50a
axel <axelselbach90@gmail.com>
2023-09-18 16:35:37 -0300

    created node js application

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,12 @@
+const appError = require("../utils/appError")
+
 class usersController {
   create(request, response) {
     const { name, email, password } = request.body
+
+    if(!name){
+      throw new appError("Nome é obrigatório!")
+    }
 
     response.status(201).json({ name, email, password })
   }
