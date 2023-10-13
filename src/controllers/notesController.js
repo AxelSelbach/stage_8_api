85dc777efe2b95ab62ad0d139d22efc4fcdbdcb7
axel <axelselbach90@gmail.com>
2023-10-13 12:30:16 -0300

    WhereIn filter

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -57,11 +57,24 @@ class notesController {
   }
 
   async index(request, response) {
-    const { user_id, title } = request.query
-    const notes = await knex("notes")
-    .where({ user_id })
-    .whereLike("title", `%${title}%`)
-    .orderBy("title")
+    const { user_id, title, tags } = request.query
+
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+      
+
+      notes = await knex("tags").whereIn("name", filterTags)
+
+    } else {
+      notes = await knex("notes")
+      .where({ user_id })
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+
+    }
+
 
     return response.json(notes)
   }
